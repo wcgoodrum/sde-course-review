bb6ff3e397d84009363a416e1d44a1e9e905749a
Chai Z <zdc4tp@virginia.edu>
2023-04-27 23:01:23 -0400

    added to the Review class

--- src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/Review.java
+++ src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/Review.java
@@ -1,9 +1,15 @@
 package edu.virginia.cs.hw7coursereviewkpb8hpzdc4tpwcg9ev;
 
+// Review class:
+// INT id (autoincrement primary key)
+// STRING text
+// INT rating
+
 public class Review {
     private int id;
     private String text;
     private int rating;
+
 
     public Review(int id, String text, int rating) {
         this.id = id;
@@ -11,7 +17,27 @@ public class Review {
         this.rating = rating;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getText() {
         return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
