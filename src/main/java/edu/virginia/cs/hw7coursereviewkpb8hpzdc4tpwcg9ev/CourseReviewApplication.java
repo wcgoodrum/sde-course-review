0074af8bebe898f94f32a76c8787b3e7fe7dff79
owens <kpb8hp@virginia.edu>
2023-05-01 16:23:35 -0400

    finished adding fx ids

--- src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/CourseReviewApplication.java
+++ src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/CourseReviewApplication.java
@@ -8,16 +8,46 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class CourseReviewApplication extends Application {
+    Scene login;
+    Scene mainMenu;
+    Scene seeReviews;
+    Scene createReview;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        FXMLLoader fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("login.fxml"));
+        login = new Scene(fxmlLoader.load(), 600, 400);
+        fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("mainMenu.fxml"));
+        mainMenu = new Scene(fxmlLoader.load(), 600, 400);
+        fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("seeReviews.fxml"));
+        seeReviews = new Scene(fxmlLoader.load(), 600, 400);
+        fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("createReview.fxml"));
+        createReview = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Course Reviews");
-        stage.setScene(scene);
+        stage.setScene(login);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    public void changeScene(Stage stage, String scene){
+        stage.setScene(getScene(scene));
+    }
+
+    public Scene getScene(String scene){
+        if (scene.equals("login")){
+            return login;
+        }
+        else if (scene.equals("mainMenu")){
+            return mainMenu;
+        }
+        else if (scene.equals("seeReviews")){
+            return seeReviews;
+        }
+        else if(scene.equals(("createReview"))){
+            return createReview;
+        }
+        throw new IllegalArgumentException("If this is thrown than one of the scene changes is spelled wrong in the controller");
     }
 }
