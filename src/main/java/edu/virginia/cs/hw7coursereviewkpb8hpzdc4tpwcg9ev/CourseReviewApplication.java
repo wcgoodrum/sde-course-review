88cd4ce35a57bfc5abd62cfc913c2174176bcb54
owens <kpb8hp@virginia.edu>
2023-05-01 18:19:20 -0400

    added reset methods

--- src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/CourseReviewApplication.java
+++ src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/CourseReviewApplication.java
@@ -8,20 +8,10 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class CourseReviewApplication extends Application {
-    Scene login;
-    Scene mainMenu;
-    Scene seeReviews;
-    Scene createReview;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("login.fxml"));
-        login = new Scene(fxmlLoader.load(), 600, 400);
-        fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("mainMenu.fxml"));
-        mainMenu = new Scene(fxmlLoader.load(), 600, 400);
-        fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("seeReviews.fxml"));
-        seeReviews = new Scene(fxmlLoader.load(), 600, 400);
-        fxmlLoader = new FXMLLoader(CourseReviewApplication.class.getResource("createReview.fxml"));
-        createReview = new Scene(fxmlLoader.load(), 600, 400);
+        Scene login = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Course Reviews");
         stage.setScene(login);
         stage.show();
@@ -29,25 +19,5 @@ public class CourseReviewApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-    }
-
-    public void changeScene(Stage stage, String scene){
-        stage.setScene(getScene(scene));
-    }
-
-    public Scene getScene(String scene){
-        if (scene.equals("login")){
-            return login;
-        }
-        else if (scene.equals("mainMenu")){
-            return mainMenu;
-        }
-        else if (scene.equals("seeReviews")){
-            return seeReviews;
-        }
-        else if(scene.equals(("createReview"))){
-            return createReview;
-        }
-        throw new IllegalArgumentException("If this is thrown than one of the scene changes is spelled wrong in the controller");
     }
 }
--- src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/CourseReviewController.java
+++ src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/CourseReviewController.java
@@ -1,6 +1,7 @@
 package edu.virginia.cs.hw7coursereviewkpb8hpzdc4tpwcg9ev;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -13,5 +14,50 @@ public class CourseReviewController {
     public TextField crCourseText, crRatingText, crMessageText, lUsernameText, lPasswordText, lConfirmText, srSearchBox;
     @FXML
     public Label crErrorLabel, lConfirmLabel, lErrorLabel, srCourseName, srReviewsLabel, srErrorLabel, srReviewLabel1, srReviewLabel2, srReviewLabel3, srReviewLabel4, srReviewLabel5, srReviewPageLabel;
+    @FXML
+    public void initialize(){
+        resetLogin();
+    }
+    public void resetLogin(){
+        resetNode(lLoginButton, false);
+        resetNode(lNewUserButton, false);
+        resetNode(lUsernameText, false);
+        resetNode(lPasswordText, false);
+        resetNode(lConfirmText,true);
+        resetNode(lConfirmLabel,true);
+        resetNode(lErrorLabel,true);
+    }
 
+    public void resetCreateReview(){
+        resetNode(crMainMenuButton, false);
+        resetNode(crCreateButton, false);
+        resetNode(crCourseText, false);
+        resetNode(crRatingText, false);
+        resetNode(crMessageText, false);
+        resetNode(crErrorLabel, true);
+    }
+
+    public void resetSeeReviews(){
+        resetNode(srSearchButton, false);
+        resetNode(srSearchBox, false);
+        resetNode(srReviewsLabel, true);
+        resetNode(srCourseName, true);
+        resetNode(srErrorLabel, true);
+        resetNode(srReviewLabel1, true);
+        resetNode(srReviewLabel2, true);
+        resetNode(srReviewLabel3, true);
+        resetNode(srReviewLabel4, true);
+        resetNode(srReviewLabel5, true);
+        resetNode(srReviewPageLabel, true);
+        resetNode(srReviewBackButton, true);
+        resetNode(srReviewForwardButton, true);
+    }
+
+    public void resetNode(Node node, boolean value){
+        node.setVisible(!value);
+        node.setDisable(value);
+        if(node.getClass() == TextField.class){
+            ((TextField) node).setText("");
+        }
+    }
 }
