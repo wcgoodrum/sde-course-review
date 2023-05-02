a4be81cf25426f8d44d12e6b8b677bab74157487
owens <kpb8hp@virginia.edu>
2023-05-02 12:41:35 -0400

    commit so I can reset main

--- src/main/java/edu/virginia/cs/hw7coursereviewkpb8hpzdc4tpwcg9ev/CourseReviewController.java
+++
@@ -1,9 +0,0 @@
-package edu.virginia.cs.hw7coursereviewkpb8hpzdc4tpwcg9ev;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-public class CourseReviewController {
-
-    private Student student;
-
-}
